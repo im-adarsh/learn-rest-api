3e290bf4e6fd75345a2669c6367a91f89dabdf75
Adarsh Kumar <clydoskope@gmail.com>
2018-10-16 02:07:54 +0800

    adding swagger ui

---
+++ src/main/java/com/clydoskope/code/learnrestapi/api/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.clydoskope.code.learnrestapi.api.config;
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public WebMvcConfigurerAdapter adapter() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("swagger-ui.html")
+                        .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+                registry.addResourceHandler("/webjars/**")
+                        .addResourceLocations("classpath:/META-INF/resources/webjars/");
+                super.addResourceHandlers(registry);
+            }
+        };
+    }
+}
